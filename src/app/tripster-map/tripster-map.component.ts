d3156b2a4bc2bb9f87553ed772176198cfdb4dff
kreuzerk <kevin.kreuzer90@icloud.com>
2017-07-26 17:22:22 +0200

    Draw path

--- src/app/tripster-map/tripster-map.component.ts
+++ src/app/tripster-map/tripster-map.component.ts
@@ -2,6 +2,7 @@
  * Created by kevinkreuzer on 25.07.17.
  */
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {LatLngLiteral} from '@agm/core';
 
 @Component({
     selector: 'tripster-map',
@@ -14,6 +15,11 @@ export class TripsterMapComponent implements OnInit {
     lat = 46.288994
     lng = 7.9368406
     stops = []
+    paths: Array<LatLngLiteral> = [
+        {lat: 46.288994, lng: 7.9368406},
+        {lat: 47.3775499, lng: 8.4666756},
+        {lat: 47.3670099, lng: 9.7004806}
+    ]
 
     constructor() {
     }
@@ -21,6 +27,7 @@ export class TripsterMapComponent implements OnInit {
     ngOnInit(): void {
         this.stops.push(this.createStop(46.288994, 7.9368406))
         this.stops.push(this.createStop(47.3775499, 8.4666756))
+        this.stops.push(this.createStop(47.3670099, 9.7004806))
     }
 
     private createStop(lat: number, lng: number): any {
