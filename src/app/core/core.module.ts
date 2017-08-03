31bac49e8b20d65d88564c655673fcbf151b321e
kreuzerk <kevin.kreuzer90@icloud.com>
2017-08-03 17:37:40 +0200

    Add navbar

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by kevinkreuzer on 03.08.17.
+ */
+import {NgModule} from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component';
+
+@NgModule({
+    declarations: [NavbarComponent],
+    exports: [NavbarComponent]
+})
+export class CoreModule {
+}
---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by kevinkreuzer on 03.08.17.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'tripster-navbar',
+    templateUrl: './navbar.html'
+})
+export class NavbarComponent {
+}
