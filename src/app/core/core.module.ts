661e3720318c81038ef9a3b8a919c046879668d7
kreuzerk <kevin.kreuzer90@icloud.com>
2017-08-03 17:39:45 +0200

    Add module import guard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 /**
  * Created by kevinkreuzer on 03.08.17.
  */
-import {NgModule} from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {NavbarComponent} from './navbar/navbar.component';
+import {throwIfAlreadyLoaded} from './module-import-guard';
 
 @NgModule({
     declarations: [NavbarComponent],
     exports: [NavbarComponent]
 })
 export class CoreModule {
+
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        throwIfAlreadyLoaded(parentModule, 'core module');
+    }
 }
