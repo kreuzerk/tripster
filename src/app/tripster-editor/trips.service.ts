fba2b953d24416e2f900ccdd09aa6d8a89583a37
kreuzerk <kevin.kreuzer90@icloud.com>
2017-08-05 23:10:40 +0200

    Retrieve data from firebase

--- src/app/tripster-editor/trips.service.ts
+++ src/app/tripster-editor/trips.service.ts
@@ -5,11 +5,17 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import {TripsterDestination} from './tipster-inputs/tripster-destination/tripster-destination.model';
+import {AngularFireDatabase} from 'angularfire2/database';
 
 @Injectable()
 export class TripService {
 
     private trips$ = new Subject<TripsterDestination>()
+
+    constructor(private database: AngularFireDatabase) {
+        this.database.list('destinations')
+            .subscribe(destinations => console.log('Retrieved destinations', destinations))
+    }
 
     public addDestination(destination: TripsterDestination) {
         this.trips$.next(destination)
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by kevinkreuzer on 05.08.17.
+ */
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDifYopTZsywG5l0CYlWsXu_V9VO_44SJ8',
+    authDomain: 'tripster-87dda.firebaseapp.com',
+    databaseURL: 'https://tripster-87dda.firebaseio.com',
+    storageBucket: 'tripster-87dda.appspot.com',
+};
